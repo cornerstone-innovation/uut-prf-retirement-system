59dad3da0a66fc25f475360067f2b581d263927d
cornerstone innovation <connerston.io@gmail.com>
2026-04-17 13:46:59 +0300

    store plan request

--- app/Http/Requests/Plan/StorePlanCashPositionRequest.php
+++ app/Http/Requests/Plan/StorePlanCashPositionRequest.php
@@ -6,23 +6,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StorePlanCashPositionRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'position_date' => ['required', 'date'],
+            'cash_amount' => ['required', 'numeric', 'gte:0'],
+            'source_type' => ['nullable', 'string', 'max:100'],
+            'notes' => ['nullable', 'string'],
         ];
     }
 }
